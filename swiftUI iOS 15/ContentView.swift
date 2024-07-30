eb118646d3910db03a938dbd6067dc70f7f4e2f1
BdriahFakhrji <bdriahtalaat@gmail.com>
2024-07-30 08:10:54 +0300

    Initial Commit

---
+++ swiftUI iOS 15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI iOS 15
+//
+//  Created by Bdriah Talaat on 24/01/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI iOS 15/swiftUI_iOS_15App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_iOS_15App.swift
+//  swiftUI iOS 15
+//
+//  Created by Bdriah Talaat on 24/01/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_iOS_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
